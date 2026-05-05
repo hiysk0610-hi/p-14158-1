08dcd15351a3cbd2dfa5f35576508ce13fe851f7
hiysk0610-hi <hiysk0610@gmail.com>
2026-05-05 16:47:13 +0900

    final

--- src/main/java/com/back/domain/post/post/entity/Post.java
+++ src/main/java/com/back/domain/post/post/entity/Post.java
@@ -1,10 +1,7 @@
 package com.back.domain.post.post.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.hibernate.sql.ast.tree.expression.JsonTableColumnDefinition;
 
 @Entity // 아래 구조대로 DB 테이블을 만들어야 한다.
@@ -12,20 +9,22 @@ import org.hibernate.sql.ast.tree.expression.JsonTableColumnDefinition;
 @Setter // 자동으로 만들어줌
 //@AllArgsConstructor 이건 모든 생성자를 받는 어노테이션
 //title과 content 만 골라서 받는 생성자가 필요하다 .
-@RequiredArgsConstructor // 얘는 final 붙은것만 골라서 해준다.
+//@RequiredArgsConstructor // 얘는 final 붙은것만 골라서 해준다.
+@NoArgsConstructor
 public class Post {
     @Id //이 컬럼이 PK(기본키)입니다”라고 JPA한테 알려주는 표시
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-    private  String title;
+    private final int id = 0; // id는 값이 변하지 말아야 한다. 그치만 RequiredArgsConstructor 붙일 수 없어서 =0을 넣어줌으로써 사용 가능하다.
+    private String title; // 제목과 내용은 수정이 가능하니깐 final 붙이지 않는게 좋다.
     @Column(columnDefinition = "TEXT")
-    private  String content;
+    private String content;
 
     public Post(String title, String content) {
         this.title = title;
         this.content = content;
     }
 }
+
 
 
 //@Id → “이게 주민번호야” (기준 지정)
--- src/main/java/com/back/global/initData/BaseInitData.java
+++ src/main/java/com/back/global/initData/BaseInitData.java
@@ -7,6 +7,9 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.boot.ApplicationArguments;
+
+import java.util.Optional;
+
 @Configuration
 
 public class BaseInitData {
@@ -15,16 +18,24 @@ public class BaseInitData {
 
     @Bean
     ApplicationRunner BaseInitDataAppLicationRunner() {
-        return  args -> {
-         //   if (postRepository.count()>0) return; //데이터가 있으면 =0이상이면 리턴
+        return args -> {
+            work1();
+            work2();
+        };
+    }
+         void work1() {
+                if (postRepository.count()>0) return; //데이터가 있으면 =0이상이면 리턴
 
-            Post post1 = postRepository.save(new Post("제목1", "내용1"));
-            Post post2 = postRepository.save(new Post("제목2", "내용2"));
-            System.out.println("기본 데이터가 초기화되었습니다.");
+                Post post1 = postRepository.save(new Post("제목1", "내용1"));
+                Post post2 = postRepository.save(new Post("제목2", "내용2"));
+                System.out.println("기본 데이터가 초기화되었습니다.");
+            }
+        void work2() {
+            Optional<Post>opPost1 = postRepository.findById(1);
 
 
-            //   postRepository.count();
+                //   postRepository.count();
             //SELECT COUNT(*) FROM post; 이거랑 동일하다. count()는 “DB 잘 붙었는지 확인용 테스트 코드”
          };
     }
-}
+
