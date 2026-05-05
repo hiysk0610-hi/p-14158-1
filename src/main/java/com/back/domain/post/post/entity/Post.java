0dad0e38f7c7b679bc0ca6edb7c1533cbad6e7a6
hiysk0610-hi <hiysk0610@gmail.com>
2026-05-05 15:58:01 +0900

    @RequiredArgsConstructor 와 final 의 조합으로 특정 필드만 초기화하는 생성자를 Post 클래스에 추가

--- src/main/java/com/back/domain/post/post/entity/Post.java
+++ src/main/java/com/back/domain/post/post/entity/Post.java
@@ -1,22 +1,30 @@
 package com.back.domain.post.post.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import org.hibernate.sql.ast.tree.expression.JsonTableColumnDefinition;
 
 @Entity // 아래 구조대로 DB 테이블을 만들어야 한다.
 @Getter // 자동으로 만들어줌
 @Setter // 자동으로 만들어줌
+//@AllArgsConstructor 이건 모든 생성자를 받는 어노테이션
+//title과 content 만 골라서 받는 생성자가 필요하다 .
+@RequiredArgsConstructor // 얘는 final 붙은것만 골라서 해준다.
 public class Post {
     @Id //이 컬럼이 PK(기본키)입니다”라고 JPA한테 알려주는 표시
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    private String title;
-    private String content;
+    private  String title;
+    @Column(columnDefinition = "TEXT")
+    private  String content;
 
+    public Post(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
 }
 
 
--- src/main/java/com/back/global/initData/BaseInitData.java
+++ src/main/java/com/back/global/initData/BaseInitData.java
@@ -1,5 +1,6 @@
 package com.back.global.initData;
 
+import com.back.domain.post.post.entity.Post;
 import com.back.domain.post.post.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
@@ -15,9 +16,14 @@ public class BaseInitData {
     @Bean
     ApplicationRunner BaseInitDataAppLicationRunner() {
         return  args -> {
+         //   if (postRepository.count()>0) return; //데이터가 있으면 =0이상이면 리턴
+
+            Post post1 = postRepository.save(new Post("제목1", "내용1"));
+            Post post2 = postRepository.save(new Post("제목2", "내용2"));
             System.out.println("기본 데이터가 초기화되었습니다.");
 
-            postRepository.count();
+
+            //   postRepository.count();
             //SELECT COUNT(*) FROM post; 이거랑 동일하다. count()는 “DB 잘 붙었는지 확인용 테스트 코드”
          };
     }
